e1d888e1c859b23c675a795e69b7f49df07074e2
João Daniel <dhanauer@gmail.com>
2021-02-16 23:48:54 -0300

    Aumento da class usuarios

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -55,6 +55,47 @@ class Usuario {
 		}
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+
+		$sql = new sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+		$sql = new sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+	));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new dateTime($row['dtcadastro']));
+
+		}else{
+
+			throw new Exception("Login e/ou Senhha inválidos.");
+			
+		}
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 
 require_once("config.php");
 
+
+/*Retorna apenas 1 usuário
 $root = new Usuario();
 $root->loadById(3);
+echo $root*/
 
-echo $root;
+/*Retorna uma lista de usuários
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+/*Carrega uma lista de user pelo Login
+$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//Carrega user login e senha
+$usuario = new Usuario();
+$usuario->login("Joao","!@jhgjh2");
+Echo $usuario;
+
 
 ?>
