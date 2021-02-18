cf363faf9b3ba2f15fce7d59d825ca8d92b6d439
João Daniel <dhanauer@gmail.com>
2021-02-18 16:43:22 -0300

    Added inset e update

--- index.php
+++ index.php
@@ -16,10 +16,25 @@ echo json_encode($lista);*/
 $search = Usuario::search("jo");
 echo json_encode($search);*/
 
-//Carrega user login e senha
+/*Carrega user login e senha
 $usuario = new Usuario();
 $usuario->login("Joao","!@jhgjh2");
-Echo $usuario;
+Echo $usuario;*/
 
+
+/*Criando nov user no banco com insert
+$aluno = new Usuario("Aluno2", "!@#12");
+//$aluno->setDeslogin("Aluno");
+//$aluno->setDessenha("1234123");
+$aluno->insert();
+echo $aluno; */
+
+//update de registro no banco
+
+$usuario = new Usuario();
+$usuario->loadById(6);
+$usuario->update("professor", "!@#$%¨&");
+
+echo $usuario;
 
 ?>
